e8a8884e8a0fd4b61c027ee5e560ae1ce7836ba5
Kimberley <Kimberley@geomes-MacBook-Pro.local>
2020-05-14 21:49:21 +0100

    add comments displayUserList

--- src/components/Layout/DisplayUserList/__tests__/DisplayUserList.test.js
+++ src/components/Layout/DisplayUserList/__tests__/DisplayUserList.test.js
@@ -55,6 +55,7 @@ describe("DisplayUserList", () => {
   beforeEach(() => {
     wrapper = mount(<DisplayUserList {...initialProps} />);
     cardNode = () => wrapper.queryByTestId("card-container");
+    // cardNode = () => wrapper.find([data-testid=card-container])
   });
 
   afterEach(() => {
@@ -68,7 +69,9 @@ describe("DisplayUserList", () => {
 
   it("initially renders no data", () => {
     const nodata = wrapper.queryByTestId("no-data");
+    // const nodata =  wrapper.find([data-testid=no-data])
     expect(nodata).toBeInTheDocument();
+    // expect(nodata.exists()).toBeTruthy()
   });
 
   describe("when data is present", () => {
@@ -77,24 +80,31 @@ describe("DisplayUserList", () => {
     });
     it("renders the card when there is no editing ID", () => {
       expect(cardNode()).toBeInTheDocument();
+      // expect(cardNode().exists()).toBeTruthy()
     });
 
     it("calls deleteUser when the delete button is clicked", () => {
       const deleteButton = wrapper.queryByTestId("delete");
+      // const deleteButton =  wrapper.find([data-testid=delete])
       fireEvent.click(deleteButton);
+      // deleteButton.simulate("click")
       expect(deleteUser).toHaveBeenCalledWith("1");
     });
 
     it("calls handleEditClick when the edit button is clicked", () => {
       const editButton = wrapper.queryByTestId("edit");
+      // const editButton =  wrapper.find([data-testid=edit])
       fireEvent.click(editButton);
+      // editButton.simulate("click")
       expect(handleEditClick).toHaveBeenCalledWith("1");
     });
 
     it("renders the userForm when there is an editing ID", () => {
       wrapper.setProps(nextNextProps);
       const userForm = wrapper.queryByTestId("user-form");
+      // const userForm =  wrapper.find([data-testid=user-form])
       expect(userForm).toBeInTheDocument();
+      // expect(userForm.exists()).toBeTruthy()
     });
   });
 });
