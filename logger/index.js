8f476802b286a0c20e746c51757a01e2fbc715b2
Matt <carlotta.matt@gmail.com>
2020-11-03 15:31:08 -0800

    [skip-ci] Update logger

--- logger/index.js
+++ logger/index.js
@@ -1,22 +1,22 @@
-/* eslint-disable no-console */
 const chalk = require("chalk");
 
+const { log } = console;
+
 const logErrorMessage = error =>
-  console.log(
+  log(
     `\n${chalk.rgb(255, 255, 255).bgRgb(255, 17, 0)(" ERROR ")} ${chalk.red(
-      `${error}`,
-    )}\n`,
+      `${error}`
+    )}\n`
   );
 
 const logInfoMessage = message =>
-  console.log(
+  log(
     `\n${chalk.rgb(7, 54, 66).bgRgb(38, 139, 210)(" INFO ")} ${chalk.blue(
-      `${message}`,
-    )}`,
+      `${message}`
+    )}`
   );
 
 module.exports = {
   logErrorMessage,
-  logInfoMessage,
+  logInfoMessage
 };
-/* eslint-enable no-console */
